9ba19e5aa8b7863de41752325e21d8aadb94996b
Priyankwof <priyanksonu123@gmail.com>
2016-10-29 21:30:03 -0400

    added class constructor
    Defined class variable sand constructor.

--- src/org/wof/foodministry/FoodEntry.java
+++ src/org/wof/foodministry/FoodEntry.java
@@ -1,5 +1,15 @@
 package org.wof.foodministry;
 
 public class FoodEntry {
-
+	String day;
+	String time;
+	int weight;
+	int waste;
+	
+	FoodEntry(String day, String time, int weight, int waste) {
+		this.day = day;
+		this.time = time;
+		this.weight = weight;
+		this.waste = waste;
+	}
 }
