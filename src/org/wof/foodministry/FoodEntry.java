715bf3f44cec1bf98fd6c783f6155b36d4ff45dd
Priyankwof <priyanksonu123@gmail.com>
2016-10-29 21:21:37 -0400

    Created project classes
    The project has two classes:
    - FoodMinistryConsoleApp - main application
    - FoodEntry - class object that holds data for each day

---
+++ src/org/wof/foodministry/FoodEntry.java
@@ -0,0 +1,5 @@
+package org.wof.foodministry;
+
+public class FoodEntry {
+
+}
---
+++ src/org/wof/foodministry/FoodMinistryConsoleApp.java
@@ -0,0 +1,10 @@
+package org.wof.foodministry;
+
+public class FoodMinistryConsoleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
